04147a3eca5d0ee3d24dacc3cf0e7fe815e03a7f
Kiên Võ <kienvo@Kiens-MacBook-Air.local>
2023-11-11 22:13:04 +0700

    add docker file

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import { StyledEngineProvider } from "@mui/material/styles";
-import { AxiosInterceptor } from "api/axiosClient";
-import { store } from "app/store";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import Routers from "routers";
-import "_styles.scss";
+import { StyledEngineProvider } from '@mui/material/styles';
+import { AxiosInterceptor } from 'api/axiosClient';
+import { store } from 'app/store';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import Routers from 'routers';
+import '_styles.scss';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <StyledEngineProvider injectFirst>
